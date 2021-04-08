1ff0d4f620c5d94f3357146bcc23d03cd32c566e
iguri <gora94650@gmail.com>
2021-04-08 10:16:56 +0530

    modified:   src/Links.js 	modified:   src/index.scss

--- src/Links.js
+++ src/Links.js
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
 
 function Links(props){
-    return(<><Link to='/movies'>Movies</Link><Link to='/tv'>Tv</Link></>)
+    return(<><Link to='/movies' className='links'><div id="container">
+    <button class="learn-more">
+      <span class="circle" aria-hidden="true">
+        <span class="icon arrow"></span>
+      </span>
+      <span class="button-text">Movies</span>
+    </button>
+  </div></Link>
+  <Link to='/tv'><button class="learn-more">
+      <span class="circle" aria-hidden="true">
+        <span class="icon arrow"></span>
+      </span>
+      <span class="button-text">Tv Shows</span>
+    </button></Link></>)
 }
-
-
-
 
 export default Links;
